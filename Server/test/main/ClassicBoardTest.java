4b36b8d399f32122d90268b61d81027c085dacbb
Maciej <krol.maciej10@gmail.com>
2018-12-05 22:07:22 +0100

    Dodano klasy testujące

---
+++ Server/test/main/ClassicBoardTest.java
@@ -0,0 +1,60 @@
+package main;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClassicBoardTest
+{
+
+    @Test
+    void constructorTest()
+    {
+        ClassicBoard classicBoard = new ClassicBoard();
+        Field field = classicBoard.getField(5, 5);
+        assertFalse(field.isPlayable());
+        assertNull(field.getCurrentColor());
+        assertNull(field.getNativeColor());
+        assertNull(field.getTargetColor());
+    }
+
+    @Test
+    void addingToUnplayableFieldException()
+    {
+
+    }
+
+    @Test
+    void addPieceTest()
+    {
+        ClassicBoard classicBoard = new ClassicBoard();
+        //początkowo pole ma currentColor = "none"
+        classicBoard.setField(1, 1, new Field("none", "none", "none", true));
+        try
+        {
+            //dodajemy pionek koloru [G]reen
+            classicBoard.addPiece(1, 1, "G");
+        } catch (UnplayableFieldException ex)
+        {
+            fail();
+        }
+        assertEquals(classicBoard.getField(1, 1).getCurrentColor(), "G");
+    }
+
+    @Test
+    void removePieceTest()
+    {
+        ClassicBoard classicBoard = new ClassicBoard();
+        //początkowo pole ma currentColor = "G"
+        classicBoard.setField(1, 1, new Field("G", "none", "none", true));
+        try
+        {
+            //usuwamy pionek
+            classicBoard.removePiece(1, 1);
+        } catch (UnplayableFieldException ex)
+        {
+            fail();
+        }
+        assertEquals(classicBoard.getField(1, 1).getCurrentColor(), "none");
+    }
+}
