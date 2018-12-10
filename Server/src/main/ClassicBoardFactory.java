53790b988a119c4c26281b72ce341a84c5644763
Maciej <krol.maciej10@gmail.com>
2018-12-10 20:12:34 +0100

    Dodano możliwość tworzenia boardów dla różnej liczby graczy

--- Server/src/main/ClassicBoardFactory.java
+++ Server/src/main/ClassicBoardFactory.java
@@ -2,58 +2,193 @@ package main;
 
 import shared.PlayerColor;
 
+/**
+ * Klasa fabryka odpowiedzalna za tworzenie obiektów klasy ClassicBoard
+ */
 public class ClassicBoardFactory implements BoardFactory
 {
+    ClassicBoard board = new ClassicBoard();
+
+    /**
+     * Tworzy planszę (ClassicBoard) dla podanej liczby graczy
+     */
     @Override
-    public Board createBoard( int numberOfPlayers)
+    public Board createBoard(int numberOfPlayers)
     {
-        ClassicBoard board = new ClassicBoard();
+        //ustawienie wszystkich graczy jako nieobecnych - nie trzeba będzie tego robić w poniższym switch'u
+        setBluePlayer(false);
+        setGreenPlayer(false);
+        setOrangePlayer(false);
+        setRedPlayer(false);
+        setVioletPlayer(false);
+        setYellowPlayer(false);
+
         switch (numberOfPlayers)
         {
-        case 2: // plansza dla 2 graczy
-            //górny promień
-            board.setField(7, 1, new Field(PlayerColor.GREEN, PlayerColor.GREEN, PlayerColor.RED, true));
-            board.setField(6, 2, new Field(PlayerColor.GREEN, PlayerColor.GREEN, PlayerColor.RED, true));
-            board.setField(7, 2, new Field(PlayerColor.GREEN, PlayerColor.GREEN, PlayerColor.RED, true));
-            board.setField(6, 3, new Field(PlayerColor.GREEN, PlayerColor.GREEN, PlayerColor.RED, true));
-            board.setField(7, 3, new Field(PlayerColor.GREEN, PlayerColor.GREEN, PlayerColor.RED, true));
-            board.setField(8, 3, new Field(PlayerColor.GREEN, PlayerColor.GREEN, PlayerColor.RED, true));
-            board.setField(5, 4, new Field(PlayerColor.GREEN, PlayerColor.GREEN, PlayerColor.RED, true));
-            board.setField(6, 4, new Field(PlayerColor.GREEN, PlayerColor.GREEN, PlayerColor.RED, true));
-            board.setField(7, 4, new Field(PlayerColor.GREEN, PlayerColor.GREEN, PlayerColor.RED, true));
-            board.setField(8, 4, new Field(PlayerColor.GREEN, PlayerColor.GREEN, PlayerColor.RED, true));
-        case 1: // plansza dla 1 gracza
-            //dolny promień
-            board.setField(7, 17, new Field(PlayerColor.RED, PlayerColor.RED, PlayerColor.GREEN, true));
-            board.setField(6, 16, new Field(PlayerColor.RED, PlayerColor.RED, PlayerColor.GREEN, true));
-            board.setField(7, 16, new Field(PlayerColor.RED, PlayerColor.RED, PlayerColor.GREEN, true));
-            board.setField(6, 15, new Field(PlayerColor.RED, PlayerColor.RED, PlayerColor.GREEN, true));
-            board.setField(7, 15, new Field(PlayerColor.RED, PlayerColor.RED, PlayerColor.GREEN, true));
-            board.setField(8, 15, new Field(PlayerColor.RED, PlayerColor.RED, PlayerColor.GREEN, true));
-            board.setField(5, 14, new Field(PlayerColor.RED, PlayerColor.RED, PlayerColor.GREEN, true));
-            board.setField(6, 14, new Field(PlayerColor.RED, PlayerColor.RED, PlayerColor.GREEN, true));
-            board.setField(7, 14, new Field(PlayerColor.RED, PlayerColor.RED, PlayerColor.GREEN, true));
-            board.setField(8, 14, new Field(PlayerColor.RED, PlayerColor.RED, PlayerColor.GREEN, true));
-            
-            //TODO implementacja dla innej liczby graczy
+            case 2: // plansza dla 2 graczy
+            {
+                setRedPlayer(true);
+                setGreenPlayer(true);
+                break;
+            }
+
+            case 3: // plansza dla 3 graczy
+            {
+                setRedPlayer(true);
+                setBluePlayer(true);
+                setYellowPlayer(true);
+                break;
+            }
+            case 4: //plansza dla 4 graczy
+            {
+                setYellowPlayer(true);
+                setOrangePlayer(true);
+                setVioletPlayer(true);
+                setBluePlayer(true);
+            }
+            case 6: //plansza dla 6 graczy
+            {
+                setRedPlayer(true);
+                setGreenPlayer(true);
+                setYellowPlayer(true);
+                setOrangePlayer(true);
+                setVioletPlayer(true);
+                setBluePlayer(true);
+                break;
+            }
+            default:
+            {
+                return null;
+            }
+
         }
 
         // Wypełnianie środkowej części planszy
-        
+
         //zmienne pomocnicze do inicjalizacji pól
         int[] beginnings = {1, 1, 2, 3, 3, 2, 2, 1, 1};
         int[] endings = {13, 12, 12, 11, 11, 11, 12, 12, 13};
         //iteracja po j - rzędach
         for (int j = 5; j <= 13; j++)
         {
-            //iteracja po kolumnach
+            //iteracja po i - kolumnach
             for (int i = beginnings[j - 5]; i <= endings[j - 5]; i++)
             {
                 board.setField(i, j, new Field(PlayerColor.NONE, PlayerColor.NONE, PlayerColor.NONE, true));
             }
         }
-        
+
         return board;
     }
+
+    /**
+     * Tworzy pola w ramieniu gracza zielonego, jeżli parametr isColorInGame == true to wypełnia stworzone pola odpowiednimi pionkami
+     */
+    private void setGreenPlayer(boolean isColorInGame)
+    {
+        //jeżeli kolor ma być grywalny to pole ma CurrentColor GREEN, w przeciwnym wypadku NONE
+        Field field = isColorInGame ? new Field(PlayerColor.GREEN, PlayerColor.GREEN, PlayerColor.RED, true) : new Field(PlayerColor.NONE, PlayerColor.GREEN, PlayerColor.RED, true);
+        board.setField(7, 1, field);
+        board.setField(6, 2, field);
+        board.setField(7, 2, field);
+        board.setField(6, 3, field);
+        board.setField(7, 3, field);
+        board.setField(8, 3, field);
+        board.setField(5, 4, field);
+        board.setField(6, 4, field);
+        board.setField(7, 4, field);
+        board.setField(8, 4, field);
+    }
+
+    /**
+     * Tworzy pola w ramieniu gracza czerwonego, jeżli parametr isColorInGame == true to wypełnia stworzone pola odpowiednimi pionkami
+     */
+    private void setRedPlayer(boolean isColorInGame)
+    {
+        Field field = isColorInGame ? new Field(PlayerColor.RED, PlayerColor.RED, PlayerColor.GREEN, true) : new Field(PlayerColor.NONE, PlayerColor.RED, PlayerColor.GREEN, true);
+        board.setField(7, 17, field);
+        board.setField(6, 16, field);
+        board.setField(7, 16, field);
+        board.setField(6, 15, field);
+        board.setField(7, 15, field);
+        board.setField(8, 15, field);
+        board.setField(5, 14, field);
+        board.setField(6, 14, field);
+        board.setField(7, 14, field);
+        board.setField(8, 14, field);
+    }
+
+    /**
+     * Tworzy pola w ramieniu gracza niebieskiego, jeżli parametr isColorInGame == true to wypełnia stworzone pola odpowiednimi pionkami
+     */
+    private void setBluePlayer(boolean isColorInGame)
+    {
+        Field field = isColorInGame ? new Field(PlayerColor.BLUE, PlayerColor.BLUE, PlayerColor.ORANGE, true) : new Field(PlayerColor.NONE, PlayerColor.BLUE, PlayerColor.ORANGE, true);
+        board.setField(1, 5, field);
+        board.setField(2, 5, field);
+        board.setField(3, 5, field);
+        board.setField(4, 5, field);
+        board.setField(1, 6, field);
+        board.setField(2, 6, field);
+        board.setField(2, 6, field);
+        board.setField(2, 7, field);
+        board.setField(3, 7, field);
+        board.setField(2, 8, field);
+    }
+
+    /**
+     * Tworzy pola w ramieniu gracza fioletowego, jeżli parametr isColorInGame == true to wypełnia stworzone pola odpowiednimi pionkami
+     */
+    private void setVioletPlayer(boolean isColorInGame)
+    {
+        Field field = isColorInGame ? new Field(PlayerColor.VIOLET, PlayerColor.VIOLET, PlayerColor.YELLOW, true) : new Field(PlayerColor.NONE, PlayerColor.VIOLET, PlayerColor.YELLOW, true);
+        board.setField(2, 10, field);
+        board.setField(2, 11, field);
+        board.setField(3, 11, field);
+        board.setField(1, 12, field);
+        board.setField(2, 12, field);
+        board.setField(3, 12, field);
+        board.setField(1, 13, field);
+        board.setField(2, 13, field);
+        board.setField(3, 13, field);
+        board.setField(4, 13, field);
+    }
+
+    /**
+     * Tworzy pola w ramieniu gracza żółtego, jeżli parametr isColorInGame == true to wypełnia stworzone pola odpowiednimi pionkami
+     */
+    private void setYellowPlayer(boolean isColorInGame)
+    {
+        Field field = isColorInGame ? new Field(PlayerColor.YELLOW, PlayerColor.YELLOW, PlayerColor.VIOLET, true) : new Field(PlayerColor.NONE, PlayerColor.YELLOW, PlayerColor.VIOLET, true);
+        board.setField(10, 5, field);
+        board.setField(11, 5, field);
+        board.setField(12, 5, field);
+        board.setField(13, 5, field);
+        board.setField(10, 6, field);
+        board.setField(11, 6, field);
+        board.setField(12, 6, field);
+        board.setField(11, 7, field);
+        board.setField(12, 7, field);
+        board.setField(11, 8, field);
+    }
+
+    /**
+     * Tworzy pola w ramieniu gracza pomaranczowego, jeżli parametr isColorInGame == true to wypełnia stworzone pola odpowiednimi pionkami
+     */
+    private void setOrangePlayer(boolean isColorInGame)
+    {
+        Field field = isColorInGame ? new Field(PlayerColor.ORANGE, PlayerColor.GREEN, PlayerColor.BLUE, true) : new Field(PlayerColor.NONE, PlayerColor.ORANGE, PlayerColor.BLUE, true);
+        board.setField(11, 10, field);
+        board.setField(11, 11, field);
+        board.setField(12, 11, field);
+        board.setField(10, 12, field);
+        board.setField(11, 12, field);
+        board.setField(12, 12, field);
+        board.setField(10, 13, field);
+        board.setField(11, 13, field);
+        board.setField(12, 13, field);
+        board.setField(13, 13, field);
+    }
+
 
 }
